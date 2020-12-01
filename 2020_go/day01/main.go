d9ccc0b5f4b2400ab00b1d43e5446121ba576b62
BrunoDavies <brunodavies@gmail.com>
2020-12-01 23:32:49 +0000

    Day01
    Did the first challange with brute force - added a cooler solution with credit. Could not do the second part with GoLang but added a solution (with credit) since I learnt a lot from it

---
+++ 2020_go/day01/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+const goal = 2020
+
+func main() {
+	allInts, err := getValueByLine("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(findTwoValuesThatSum(allInts, goal))
+
+}
+
+//This is supposed to go in /util but I do not know how to import it to this class
+//getValueByLine takes a file, reads the file and splits it by new line into a slice.
+func getValueByLine(fileName string) ([]int, error) {
+	fileContent, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	allContent := string(fileContent)
+	fileLines := strings.Split(strings.TrimSpace(allContent), "\n")
+	ints := make([]int, 0, len(fileLines))
+
+	//ensures there is no nil values returned
+	for _, value := range fileLines {
+		tempVal, err := strconv.Atoi(value)
+
+		if err != nil {
+			return nil, err
+		}
+		ints = append(ints, tempVal)
+	}
+
+	return ints, nil
+}
+
+//Solution A: from https://github.com/thlacroix/goadvent - so much cleaner / faster / interesting than mine
+func findTwoValuesThatSum(values []int, sum int) int {
+	heatmap := make(map[int]bool, len(values))
+
+	for _, v := range values {
+		if heatmap[v] {
+			return v * (sum - v)
+		}
+		heatmap[sum-v] = true
+	}
+	return 0
+}
+
+//Solution B: from https://github.com/thlacroix/goadvent - couldn't do this in GoLang (did not enter answer for star)
+//Included it since again a really clean solution that I learnt a lot from
+func threeSum(data []int, target int) int {
+	lookup := make(map[int]bool, len(data))
+
+	for _, v := range data {
+		lookup[v] = true
+	}
+	fmt.Println(lookup)
+
+	for i, x := range data {
+		for _, y := range data[i+1:] {
+			z := target - x - y
+			if lookup[z] {
+				return x * y * z
+			}
+		}
+	}
+	return 0
+}
--- 2020_go/util/fileUtil.go
+++ 2020_go/util/fileUtil.go
@@ -1,5 +1,32 @@
-package main
+package util
 
-func main() {
+import (
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
 
+//getValueByLine takes a file, reads the file and splits it by new line into a slice.
+func getValueByLine(fileName string) ([]int, error) {
+	fileContent, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	allContent := string(fileContent)
+	fileLines := strings.Split(allContent, "\n")
+	ints := make([]int, len(fileLines))
+
+	//ensures there is no nil values returned
+	for _, value := range fileLines {
+		tempVal, err := strconv.Atoi(value)
+
+		if err != nil {
+			return nil, err
+		}
+		ints = append(ints, tempVal)
+	}
+
+	return ints, nil
 }
